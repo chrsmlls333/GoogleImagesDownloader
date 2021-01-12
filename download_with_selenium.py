da1ee6a9e828754828a1825b7384cb3782ca6566
chrsmlls333 <chrsmlls333@gmail.com>
2021-01-12 01:21:52 -0600

    Massive refactor, segment out functions, add TODOs

--- download_with_selenium.py
+++ download_with_selenium.py
@@ -34,102 +34,151 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-def get_image_links(main_keyword, supplemented_keywords, link_file_path, num_requested = [0, 1000]):
+def process_google_url(url, index_start = 0, index_end = 1000):
+    """get image links with selenium
+    
+    Args:
+        url (str): google images url
+        index_start (int):
+        index_end (int):
+    
+    Returns:
+        img_urls (set[str]): a deduped set of image urls
+    """
+    # Check url for googliness
+    pass
+
+    # Check num_requested
+    # TODO Check types
+    num_requested = abs(index_end - index_start) 
+    index_start = min(index_start, index_end)
+    index_end = index_start + num_requested
+    if index_end <= 0: 
+        raise ValueError("process_google_url: 2nd arg cannot be 0. Nothing to get!")
+    number_of_scrolls = int(index_end / 400) + 1 # number_of_scrolls * 400 images will be opened in the browser
+    
+    # Report
+    print(f"process_google_url({url})")
+    print(f"Requested {num_requested} images.")
+    if index_start > 0:
+        print(f"Skipping the first {index_start} images.")
+
+    # Prepare
+    img_urls = set()
+    driver = webdriver.Firefox()
+
+    # Open page and Scan
+    driver.get(url)
+    for _ in range(number_of_scrolls):
+        for __ in range(10):
+            # multiple scrolls needed to show all 400 images
+            driver.execute_script("window.scrollBy(0, 1000000)")
+            time.sleep(2)
+        # to load next 400 images
+        time.sleep(1)
+        try:
+            driver.find_element_by_xpath("//input[@value='Show more results']").click()
+        except Exception as e:
+            print("Reached the end of page without 'Show more results' button.")
+            break
+
+    # imges = driver.find_elements_by_xpath('//div[@class="rg_meta"]') # not working anymore
+    # imges = driver.find_elements_by_xpath('//div[contains(@class,"rg_meta")]') # not working anymore
+    thumbs = driver.find_elements_by_xpath('//a[@class="wXeWr islib nfEiy mM5pbd"]')
+    print(f"Found {len(thumbs)} thumbnails.")
+
+    num_found = 0
+    for index, thumb in enumerate(thumbs):
+        if (index < index_start): 
+            continue 
+        if num_found >= num_requested: 
+            break 
+
+        try:
+            thumb.click()
+            time.sleep(1)
+        except e:
+            print("Error clicking one thumbnail.")
+
+        url_elements = driver.find_elements_by_xpath('//img[@class="n3VNCb"]')
+        for url_element in url_elements:
+            try:
+                url = url_element.get_attribute('src')
+            except e:
+                print("Error getting one url.")
+
+            if url.startswith('http') and not url.startswith('https://encrypted-tbn0.gstatic.com'):
+                img_urls.add(url)
+
+                num_found += 1
+                num_found_leading = str(num_found).zfill(math.floor(math.log10(len(thumbs))+1))
+                print(f"[{num_found_leading}] Found image url: " + url)
+
+    print(f'Found {len(img_urls)} images.')
+    driver.quit()
+
+    # Return the goods
+    return img_urls
+
+
+def write_url_list(path, text_set):
+    """write list of strings to fle as lines
+    
+    Args:
+        path (str): local file url
+        text_set (set[str]): list of strings to write
+    
+    Returns:
+        None
+    """
+    # TODO check path
+    # TODO check tuple/set/list/dict
+    # TODO Make directory
+    # Write
+    with open(path, 'w') as wf:
+        for s in text_set:
+            wf.write(s +'\n')
+
+
+def get_image_links_from_keywords(main_keyword, supplemented_keywords, link_file_path, num_requested = 1000, num_skip = 0):
     """get image links with selenium
     
     Args:
         main_keyword (str): main keyword
         supplemented_keywords (list[str]): list of supplemented keywords
         link_file_path (str): path of the file to store the links
-        num_requested (int or list[int]{2}, optional): maximum number of images to download
+        num_requested (int, optional): maximum number of images to download
+        num_skip (int, optional): first batch to skip
     
     Returns:
         None
     """
 
-    index_start = index_end = 0
-    if type(num_requested) is not list and type(num_requested) is not int:
-        raise TypeError("get_image_links: 4th arg must be of type int or list!")
-    if type(num_requested) is list: 
-        if len(num_requested) != 2:
-            raise ValueError("get_image_links: 4th arg must be of a length of 2 if list!")
-        index_start, index_end = num_requested
-        num_requested = abs(index_end - index_start) 
-    index_end = index_start + num_requested
-    if index_end <= 0: 
-        raise ValueError("get_image_links: 4th arg cannot be 0. Nothing to get!")
+    # TODO typecheck args
+
+    index_start = num_skip
+    index_end = num_skip + num_requested
 
     print(f"Requested {num_requested} images.")
     if index_start > 0:
         print(f"Skipping the first {index_start} images.")
 
-    number_of_scrolls = int(index_end / 400) + 1 
-    # number_of_scrolls * 400 images will be opened in the browser
-
     img_urls = set()
-    driver = webdriver.Firefox()
-    for i in range(len(supplemented_keywords)):
-
-        search_query = quote((main_keyword + ' ' + supplemented_keywords[i]).strip())
+    for i, sup_kw in enumerate(supplemented_keywords):
+        search_query = quote((main_keyword + ' ' + sup_kw).strip())
         url = "https://www.google.com/search?q="+search_query+"&source=lnms&tbm=isch"
-        print(f"Query: {url}")
-        driver.get(url)
-        for _ in range(number_of_scrolls):
-            for __ in range(10):
-                # multiple scrolls needed to show all 400 images
-                driver.execute_script("window.scrollBy(0, 1000000)")
-                time.sleep(2)
-            # to load next 400 images
-            time.sleep(1)
-            try:
-                driver.find_element_by_xpath("//input[@value='Show more results']").click()
-            except Exception as e:
-                print("Process-{0} reach the end of page or get the maximum number of requested images".format(main_keyword))
-                break
+        # url = url + "&tbs=ic:gray" #black and white only
 
-        # imges = driver.find_elements_by_xpath('//div[@class="rg_meta"]') # not working anymore
-        # imges = driver.find_elements_by_xpath('//div[contains(@class,"rg_meta")]') # not working anymore
-        thumbs = driver.find_elements_by_xpath('//a[@class="wXeWr islib nfEiy mM5pbd"]')
-        print("Thumbnails found: " + str(len(thumbs)))
+        img_urls |= process_google_url(url, index_start, index_end)
 
-        num_found = 0
-        for index, thumb in enumerate(thumbs):
-            if (index < index_start): 
-                continue 
-            if num_found >= num_requested: 
-                break 
+        print('get_image_links_from_keywords({0}, {1}) got {2} image urls so far.'.format(main_keyword, sup_kw, len(img_urls)))
+    print('get_image_links_from_keywords({0}) found {1} images total.'.format(main_keyword, len(img_urls)))
 
-            try:
-                thumb.click()
-                time.sleep(1)
-            except e:
-                print("Error clicking one thumbnail")
-
-            url_elements = driver.find_elements_by_xpath('//img[@class="n3VNCb"]')
-            for url_element in url_elements:
-                try:
-                    url = url_element.get_attribute('src')
-                except e:
-                    print("Error getting one url")
-
-                if url.startswith('http') and not url.startswith('https://encrypted-tbn0.gstatic.com'):
-                    img_urls.add(url)
-
-                    num_found += 1
-                    num_found_leading = str(num_found).zfill(math.floor(math.log10(len(thumbs))+1))
-                    print(f"[{num_found_leading}] Found image url: " + url)
-
-        print('Process-{0} add keyword {1} , got {2} image urls so far'.format(main_keyword, supplemented_keywords[i], len(img_urls)))
-    print('Process-{0} totally get {1} images'.format(main_keyword, len(img_urls)))
-    driver.quit()
-
-    with open(link_file_path, 'w') as wf:
-        for url in img_urls:
-            wf.write(url +'\n')
+    write_url_list(link_file_path, img_urls)
     print('Store all the links in file {0}'.format(link_file_path))
 
 
-def download_images(link_file_path, download_dir, log_dir):
+def download_link_list_file(link_file_path, download_dir, log_dir):
     """download images whose links are in the link file
     
     Args:
@@ -146,64 +195,83 @@ def download_images(link_file_path, download_dir, log_dir):
     log_file = log_dir + 'download_selenium_{0}.log'.format(main_keyword)
     logging.basicConfig(level=logging.DEBUG, filename=log_file, filemode="a+", format="%(asctime)-15s %(levelname)-8s  %(message)s")
     img_dir = download_dir + main_keyword + '/'
-    headers = {}
     if not os.path.exists(img_dir):
         os.makedirs(img_dir)
     # start to download images
     with open(link_file_path, 'r') as rf:
         for index, link in enumerate(rf):
             link = link.strip()
-            print('\n' + link)
-
-            try:
-                o = urlparse(link)
-                ref = o.scheme + '://' + o.hostname
-                #ref = 'https://www.google.com'
-                ua = generate_user_agent()
-                headers['User-Agent'] = ua
-                headers['referer'] = ref
-                # print(f'\n{link}\n{ref}\n{ua}')
-                req = urllib.request.Request(link, headers = headers)
-                response = urllib.request.urlopen(req)
-                info = response.info()
-                data = response.read()
-
-                file_name_orig = (os.path.basename(o.path))
-                file_name_orig_root, file_name_orig_ext = os.path.splitext(file_name_orig)
-                img_exts = tuple(get_extensions_for_type('image'))
-                ext_suggest = mimetypes.guess_extension(info.get_content_type())
-                if file_name_orig_ext.strip() and file_name_orig_ext.lower() in img_exts:
-                    file_name = file_name_orig
-                else:
-                    if not ext_suggest.strip():
-                        raise Exception("Found and guessed extensions not adequate... not an image?")
-                    file_name = file_name_orig_root + ext_suggest
-                    print(f"{file_name_orig_ext} -> {ext_suggest}")
-                
-                file_name = f'{index+1}_{file_name}'
-                file_path = img_dir + file_name
-
-                with open(file_path,'wb') as wf:
-                    wf.write(data)
-                print(f'Process-{main_keyword}: download image "{file_name}"')
-
-            except urllib.error.URLError as e:
-                print('URLError')
-                logging.error('URLError while downloading image {0}reason:{1}'.format(link, e.reason))
-                continue
-            except urllib.error.HTTPError as e:
-                print('HTTPError')
-                logging.error('HTTPError while downloading image {0}http code {1}, reason:{2}'.format(link, e.code, e.reason))
-                continue
-            except Exception as e:
-                print('Unexpected Error')
-                logging.error('Unexpected error while downloading image {0}error type:{1}, args:{2}'.format(link, type(e), e.args))
+            
+            print(' ')
+            success = download_image_link(link, img_dir, f'{index+1}_')
+            if not success:
                 continue
 
             # Breathe
             if (index+1) % 10 == 0:
                 print('\nProcess-{0} is sleeping...'.format(main_keyword))
                 time.sleep(5)
+
+def download_image_link(link, dir_path, prefix = ''):
+    """Use urllib to download asset
+    
+    Args:
+        link (str):
+        dir_path (str):
+        prefix (str):
+    
+    Returns:
+        bool
+    """
+    print(f'download_image_link: from "{link}"')
+
+    try:
+        o = urlparse(link)
+        ref = o.scheme + '://' + o.hostname
+        #ref = 'https://www.google.com'
+        ua = generate_user_agent()
+        headers = {}
+        headers['User-Agent'] = ua
+        headers['referer'] = ref
+        # print(f'\n{link}\n{ref}\n{ua}')
+        req = urllib.request.Request(link, headers = headers)
+        response = urllib.request.urlopen(req)
+        info = response.info()
+        data = response.read()
+
+        file_name_orig = (os.path.basename(o.path))
+        file_name_orig_root, file_name_orig_ext = os.path.splitext(file_name_orig)
+        img_exts = tuple(get_extensions_for_type('image'))
+        ext_suggest = mimetypes.guess_extension(info.get_content_type())
+        if file_name_orig_ext.strip() and file_name_orig_ext.lower() in img_exts:
+            file_name = file_name_orig
+        else:
+            if not ext_suggest.strip():
+                raise Exception("Found and guessed extensions not adequate... not an image?")
+            file_name = file_name_orig_root + ext_suggest
+            print(f"{file_name_orig_ext} -> {ext_suggest}")
+        
+        file_name = prefix + file_name
+        file_path = dir_path + file_name
+
+        with open(file_path,'wb') as wf:
+            wf.write(data)
+
+        print(f'download_image_link: to "{file_name}"')
+        return True
+
+    except urllib.error.URLError as e:
+        print('URLError')
+        logging.error('URLError while downloading image {0}reason:{1}'.format(link, e.reason))
+        return False
+    except urllib.error.HTTPError as e:
+        print('HTTPError')
+        logging.error('HTTPError while downloading image {0}http code {1}, reason:{2}'.format(link, e.code, e.reason))
+        return False
+    except Exception as e:
+        print('Unexpected Error')
+        logging.error('Unexpected error while downloading image {0}error type:{1}, args:{2}'.format(link, type(e), e.args))
+        return False
 
 def get_extensions_for_type(general_type):
     for ext in mimetypes.types_map:
@@ -265,7 +333,7 @@ if __name__ == "__main__":
     # multiple processes
     p = Pool(3) # default number of process is the number of cores of your CPU, change it by yourself
     for keyword in main_keywords:
-        p.apply_async(get_image_links, args=(keyword, supplemented_keywords, link_files_dir + keyword))
+        p.apply_async(get_image_links_from_keywords, args=(keyword, supplemented_keywords, link_files_dir + keyword, 10))
     p.close()
     p.join()
     print('Finish getting all image links')
@@ -285,7 +353,7 @@ if __name__ == "__main__":
     # multiple processes
     p = Pool() # default number of process is the number of cores of your CPU, change it by yourself
     for keyword in main_keywords:
-        p.apply_async(download_images, args=(link_files_dir + keyword, download_dir, log_dir))
+        p.apply_async(download_link_list_file, args=(link_files_dir + keyword, download_dir, log_dir))
     p.close()
     p.join()
     print('Finish downloading all images')
