b60e070469342c44fffc9d8d0d34e6f567585c20
chrsmlls333 <chrsmlls333@gmail.com>
2021-01-18 20:28:16 -0600

    Add read urls function, tidy, make write urls function return sanitized path

--- googleimagesdownloader/utils.py
+++ googleimagesdownloader/utils.py
@@ -5,6 +5,7 @@ import pathvalidate
 import hashlib
 import mimetypes
 mimetypes.init()
+from urllib.parse import urlparse
 
 
 # STRING / PATH OPS #########################################
@@ -14,11 +15,20 @@ def get_extensions_for_type(general_type):
         if mimetypes.types_map[ext].split('/')[0] == general_type:
             yield ext
 
+
 def get_query_from_google_url(url):
     query = re.findall('[?&]q=([^&]+)', url)
     if not query:
         return None
     return pathvalidate.sanitize_filename(query[0], replacement_text='_')
+
+
+def validate_url(url):
+    try:
+        result = urlparse(url)
+        return all([result.scheme, result.netloc, result.path])
+    except:
+        return False
 
 
 def get_shortcode_id(s):
@@ -32,20 +42,48 @@ def write_url_list(path, text_set):
     """write list of strings to file as lines
     
     Args:
-        path (str): local file url
+        path (str): local file path
         text_set (set[str]): list of strings to write
     
     Returns:
-        None
+        path (str): sanitized local file path
     """
-    assert isinstance(text_set, (tuple, list, set)), "text_set is not an iterable."
-    assert isinstance(path, str), "path is not a string." 
+    assert isinstance(text_set, (tuple, list, set)), "write_url_list: text_set is not an iterable."
+    assert isinstance(path, str), "write_url_list: path is not a string." 
 
     path = pathvalidate.sanitize_filepath(path, platform='universal')
 
     directory = os.path.dirname(path)
     if not os.path.exists(directory):
         os.makedirs(directory)
+
     with open(path, 'w') as wf:
         for s in text_set:
             wf.write(s +'\n')
+
+    return path
+
+
+def read_url_list(path):
+    """read file to list of strings, validate urls
+    
+    Args:
+        path (str): local file path
+    
+    Returns:
+        urls (set[str]): list of unique, validated urls
+    """
+
+    assert isinstance(path, str), "read_url_list: path is not a string." 
+    assert os.path.exists(path), "read_url_list: url list at path doesn't exist."
+
+    with open(path) as rf:
+        content = rf.read().splitlines()
+    content = set(content)
+    print(len(content))
+
+    content = filter(lambda url: validate_url(url), content)
+    print(len(content))
+
+    return content
+    
